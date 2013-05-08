712d9448cb1d4f87a33eb2243125f8a20b16412e
John McLear <john@mclear.co.uk>
2013-05-08 16:35:25 +0100

    clean up, comments and more sanity

--- stress.js
+++ stress.js
@@ -6,16 +6,13 @@ var room = url.split("/")[4]; // get the room from the url
 var serverUrl = url.split("/")[0] + "//" + url.split("/")[2]; // get the host url
 var clientCount = process.argv[3]; // get number of clients 
 var delayBetweenDrawings = process.argv[4]; // Seconds between each change
+delayBetweenDrawings = delayBetweenDrawings * 1000; // turn seconds into milliseconds..
 
-// turn sconds into ms
-delayBetweenDrawings = delayBetweenDrawings * 1000;
-
-var i = 0;
-// for each clientcount create a client which will send edits
+var i = 0; // for each clientcount create a client which will send edits
 while (i < clientCount){
-  var userid = uid();
+  var userid = uid(); // generate a unique user id
   console.log("Creating client with uid", userid);
-  createClient(uid);
+  createClient(userid); // create a client, is also responsible for sending messages
   i++;
 }
 
@@ -23,14 +20,14 @@ function createClient(userid){
   var socket = io.connect(serverUrl, {'force new connection': true});
   socket.emit('subscribe', { room: room });
 
-  setInterval(function(){
-    var path_to_send = { 
+  setInterval(function(){ // based on the delay between each edit..
+    var path_to_send = {  // create the start of an edit
       name: userid,
       rgba: {
-        blue: randColor(),
-        green: randColor(),
-        opacity: randColor(),
-        red: randColor()
+        blue: Math.random(),
+        green: Math.random(),
+        opacity: Math.random(),
+        red: Math.random()
       },
       start: {
         x: randCoOrd(),
@@ -44,32 +41,25 @@ function createClient(userid){
         "bottom": ["Point", randCoOrd(), randCoOrd()]
       }]
    }
-    socket.emit('draw:progress', room, userid, JSON.stringify(path_to_send));
-    path_to_send.path = [],
-    path_to_send.end = {
+    socket.emit('draw:progress', room, userid, JSON.stringify(path_to_send)); // send the start of an edit
+    path_to_send.path = []; // blank the paths
+    path_to_send.end = { // create end co-ords
       x: randCoOrd(),
       y: randCoOrd()
     }
   
-    console.log("Sent Path");
-    socket.emit('draw:end', room, userid, JSON.stringify(path_to_send));
+    console.log("Sent Path to server for userid:", userid);
+    socket.emit('draw:end', room, userid, JSON.stringify(path_to_send)); // send the end event
   }, delayBetweenDrawings);
 }
 
-
-function randColor(){
-  return Math.random();
-}
- 
-function randCoOrd(){
+function randCoOrd(){ // generates random coords within xy 1000
   return Math.floor(Math.random()*1000)
 }
 
-function uid() {
+function uid(){ // generates a random uid
   var S4 = function () {
     return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
   };
   return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
 }
-//());
-
